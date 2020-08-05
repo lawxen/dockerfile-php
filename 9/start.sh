90b7955ca2dc71c53d584c8a8dbc4e5bc3d4593e
qwe <lawxen>
2020-08-05 20:54:23 +0800

    Update

--- 9/start.sh
+++ 9/start.sh
@@ -4,12 +4,12 @@ php-fpm
 
 HTMLDIR=/var/www/html
 if [ "`ls -A ${HTMLDIR}`" = "" ]; then
-  if [ -d "/var/www/drupal9-download" ]; then
-    mv /var/www/drupal9-download/* /var/www/html/
-    mv /var/www/drupal9-download/.[!.]* /var/www/html/
+  if [ -d "/var/www/drupal-download" ]; then
+    mv /var/www/drupal-download/* /var/www/html/
+    mv /var/www/drupal-download/.[!.]* /var/www/html/
   fi
 fi
 
-if [ -d "/var/www/drupal9-download" ]; then
-  rm -rf /var/www/drupal9-download
+if [ -d "/var/www/drupal-download" ]; then
+  rm -rf /var/www/drupal-download
 fi
