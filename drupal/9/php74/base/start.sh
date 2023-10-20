12f229159a9fe91f81ac63e9bc35f27f60cfd358
lawxen <lawxen@outlook.com>
2023-10-20 13:08:31 +0800

    update start.sh for php7

--- drupal/9/php74/base/start.sh
+++ drupal/9/php74/base/start.sh
@@ -1,6 +1,23 @@
  #!/bin/sh
 sed -i "s#var/www/html/web#var/www/html/${DRUPAL_WEB_ROOT:-web}#g" /etc/nginx/sites-enabled/default.conf
 
+# Override server port for xdebug frendly on multiple sites debug simultaneously
+if [ -n "${INSIDE_WEB_PORT}" ]; then
+    sed -i "s#80#${INSIDE_WEB_PORT}#g" /etc/nginx/sites-enabled/default.conf
+fi
+
+# Override server name for xdebug frendly on multiple sites debug simultaneously
+if [ -n "${SERVER_NAME}" ]; then
+    sed -i "s#server_name\ \_#server_name\ ${SERVER_NAME}#g" /etc/nginx/sites-enabled/default.conf
+fi
+
+# Override xdebug port for xdebug frendly on multiple sites debug simultaneously
+if [ -n "${XDEBUG_CLIENT_PORT}" ]; then
+    xdebugConfigPath='/Users/wangzhenzhu/PycharmProjects/testPro/t.bash'
+    if [ ! -d "$xdebugConfigPath" ]; then
+        echo "xdebug.client_port = ${XDEBUG_CLIENT_PORT}" >> $xdebugConfigPath
+    fi
+fi
 service rsyslog start
 service nginx start
 php-fpm
