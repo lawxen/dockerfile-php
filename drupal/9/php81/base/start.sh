cceb189d385f2874c0fe9222f4de0f0349f426e7
lawxen <lawxen@outlook.com>
2023-10-23 09:54:49 +0800

    Update start.sh

--- drupal/9/php81/base/start.sh
+++ drupal/9/php81/base/start.sh
@@ -1,6 +1,28 @@
  #!/bin/sh
-sed -i "s#var/www/html/web#var/www/html/${DRUPAL_WEB_ROOT:-web}#g" /etc/nginx/sites-enabled/default.conf
+sed -i "s#var/www/html/web#var/www/html/${DRUPAL_WEB_ROOT:-web}#" /etc/nginx/sites-enabled/default.conf
 
+# Override server name for xdebug frendly on multiple sites debug simultaneously
+if [ -n "${SERVER_NAME}" ]; then
+    sed -i "s/server_name _;/server_name ${SERVER_NAME};/" /etc/nginx/sites-enabled/default.conf
+fi
+
+# Override server port for xdebug frendly on multiple sites debug simultaneously
+if [ -n "${INSIDE_WEB_PORT}" ]; then
+    sed -i "s/listen 80 default_server/listen ${INSIDE_WEB_PORT} default_server/" /etc/nginx/sites-enabled/default.conf
+fi
+
+# Override xdebug port for xdebug frendly on the situation: multiple sites be debugged simultaneously
+if [ -n "${XDEBUG_CLIENT_PORT}" ]; then
+    xdebugConfigPath='/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini'
+
+    if [ -e $xdebugConfigPath ]; then
+        if grep -q "xdebug.client_port" $xdebugConfigPath; then
+            sed -i "s/^xdebug.client_port=.*$/xdebug.client_port=${XDEBUG_CLIENT_PORT}/" a.conf
+        else
+            echo "xdebug.client_port=${XDEBUG_CLIENT_PORT}" >> $xdebugConfigPath
+        fi
+    fi
+fi
 service rsyslog start
 service nginx start
 php-fpm
