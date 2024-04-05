f01d674137e930d69d658243d29bbe7fc3568564
lawxen <lawxen@outlook.com>
2024-04-05 12:05:49 +0800

    Rename to template

--- drupal/debian/php80.sh
+++ drupal/debian/php80.sh
@@ -1,4 +1,4 @@
 PHP_VERSION=8.0
 DRUPAL_VERSION=9
-docker buildx build --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=${DRUPAL_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian --push .
-docker buildx build --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=${DRUPAL_VERSION} --build-arg KEEP_DRUPAL_FILES=1 -t lawxen/drupal:php${PHP_VERSION}-debian-d${DRUPAL_VERSION} --push .
+docker buildx build -f Dockerfile.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=${DRUPAL_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian --push .
+docker buildx build -f Dockerfile.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=${DRUPAL_VERSION} --build-arg KEEP_DRUPAL_FILES=1 -t lawxen/drupal:php${PHP_VERSION}-debian-d${DRUPAL_VERSION} --push .
--- drupal/debian/php82-dev.sh
+++ drupal/debian/php82-dev.sh
@@ -1,4 +1,4 @@
 PHP_VERSION=8.2
 DRUPAL_VERSION=10
-docker buildx build -f Dockerfile-dev --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian-dev --push .
-docker buildx build -f Dockerfile-dev --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=-d${DRUPAL_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian-d${DRUPAL_VERSION}-dev --push .
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian-dev --push .
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=${PHP_VERSION} --build-arg MY_DRUPAL_VERSION=-d${DRUPAL_VERSION} -t lawxen/drupal:php${PHP_VERSION}-debian-d${DRUPAL_VERSION}-dev --push .
