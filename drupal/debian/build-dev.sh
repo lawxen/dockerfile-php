f7008a0afa24eed1bf499127b2fd722bbd776359
lawxen <lawxen@outlook.com>
2024-04-06 23:35:39 +0800

    reconstruct

--- drupal/debian/build-dev.sh
+++ drupal/debian/build-dev.sh
@@ -1,18 +1,18 @@
+# PHP7.4
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php7.4-fpm -t lawxen/drupal:php7.4-fpm-dev --push .
+
+# PHP8.0
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php8.0-fpm -t lawxen/drupal:php8.0-fpm-dev --push .
+
+# PHP8.1
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php8.1-fpm -t lawxen/drupal:php8.1-fpm-dev --push .
+
+# PHP8.2
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php8.2-fpm -t lawxen/drupal:php8.2-fpm-dev --push .
+
+# PHP8.3
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php8.3-fpm -t lawxen/drupal:php8.3-fpm-dev --push .
+docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg TAG_FROM=php8.3-fpm --build-arg KEEP_DRUPAL_FILES=1 -t lawxen/drupal:10-php8.3-fpm-dev --push .
+
 # This is for local test only
-# docker buildx build --load -f Dockerfile-dev.template --platform linux/arm64 --build-arg MY_PHP_VERSION=8.3 -t lawxen/drupal:php8.3-debian-dev .
-
-# Php7.4
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=7.4 --build-arg XDEBUG_VERSION=-3.1.5 -t lawxen/drupal:php7.4-debian-dev --push .
-
-# Php8.0
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=8.0 -t lawxen/drupal:php8.0-debian-dev --push .
-
-# Php8.1
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=8.1 -t lawxen/drupal:php8.1-debian-dev --push .
-
-# Php8.2
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=8.2 -t lawxen/drupal:php8.2-debian-dev --push .
-
-# Php8.3
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=8.3 -t lawxen/drupal:php8.3-debian-dev --push .
-docker buildx build -f Dockerfile-dev.template --platform linux/amd64,linux/arm64 --build-arg MY_PHP_VERSION=8.3 --build-arg MY_DRUPAL_VERSION=-d10 -t lawxen/drupal:php8.3-debian-d10-dev --push .
+# docker buildx build --load -f Dockerfile-dev.template --platform inux/arm64 --build-arg TAG_FROM=php8.3-fpm -t lawxen/drupal:php8.3-fpm-dev .
